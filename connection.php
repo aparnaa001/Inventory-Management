438712d530e2495fc4363a85cbe49573b3e28d77
Aparna Sasi <156825551+aparnaa001@users.noreply.github.com>
2025-06-04 17:43:36 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$con = new mysqli("localhost",'root','','inventory');
+$con = new mysqli("db.be-mons1.bengt.wasmernet.com",'037b8bf47ac48000a8c010a6f2ee','0684037b-8bf4-7c32-8000-e2c293f0028f','inventory');
 ?>
